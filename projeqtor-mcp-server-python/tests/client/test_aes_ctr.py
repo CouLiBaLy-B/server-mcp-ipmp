ee68463e2dcac8b6b2aea17e11eb507c0cb617b8
bourahima.coulibaly@sii.fr <bourahima.coulibaly@sii.fr>
2026-06-22 15:25:58 +0200

    Python version

--- projeqtor-mcp-server-python/tests/client/test_aes_ctr.py
+++
@@ -1,9 +0,0 @@
-from projeqtor_mcp_server.client.aes_ctr import encrypt_aes_ctr
-
-
-def test_encrypt_aes_ctr_envelope_is_iv_and_ciphertext() -> None:
-    encrypted = encrypt_aes_ctr('{"name":"x"}', "secret", 128)
-    iv, ciphertext = encrypted.split(":")
-    assert iv
-    assert ciphertext
-    assert encrypted != '{"name":"x"}'
